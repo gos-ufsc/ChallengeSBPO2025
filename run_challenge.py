79223efd57217ac6e93d8e6b18570780cd5d405c
PedroLagarto <antunespedromh@gmail.com>
2025-09-01 22:11:38 -0300

    path update

--- run_challenge.py
+++ run_challenge.py
@@ -4,8 +4,8 @@ import sys
 import platform
 
 # Paths to the libraries
-CPLEX_PATH="/opt/ibm/ILOG/CPLEX_Studio2211/cplex/bin/x86-64_linux/"
-OR_TOOLS_PATH = "$HOME/Documents/or-tools/build/lib/"
+CPLEX_PATH="$HOME/CPLEX_Studio2211/opl/bin/arm64_osx/"
+OR_TOOLS_PATH="$HOME/Documents/or-tools/build/lib/"
 
 USE_CPLEX = True
 USE_OR_TOOLS = False
